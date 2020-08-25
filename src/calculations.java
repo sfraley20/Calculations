7b3fd9fadc37a50fc3d22b62926a4a5cc1c3c7eb
sfraley20 <s.fraley.05@gmail.com>
2020-08-25 09:26:54 -0400

    Calculations

---
+++ src/calculations.java
@@ -0,0 +1,11 @@
+public class calculations {
+    public static void main(String[] args) {
+        int firstNumber = 5;
+        int secondNumber = 4;
+
+        System.out.println("5 + 4 =" + (firstNumber + secondNumber));
+
+        System.out.println("5 * 4 =" + (firstNumber * secondNumber));
+
+    }
+}
