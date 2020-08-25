c240b7c002c9bc2f7edf5ebf1ffd1c08ae6dc00d
sfraley20 <s.fraley.05@gmail.com>
2020-08-25 10:40:46 -0400

    calculations update

--- src/calculations.java
+++ src/calculations.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 public class calculations {
     public static void main(String[] args) {
-        int firstNumber = 5;
-        int secondNumber = 4;
+//        int firstNumber = 5;
+//        int secondNumber = 4;
+//
+//        System.out.println("5 + 4 =" + (firstNumber + secondNumber));
+//
+//        System.out.println("5 * 4 =" + (firstNumber * secondNumber));
 
-        System.out.println("5 + 4 =" + (firstNumber + secondNumber));
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int firstNumber = userInput.nextInt();
+        System.out.println("Please enter another number: ");
+        int secondNumber = userInput.nextInt();
+        int sum = firstNumber + secondNumber;
+//        System.out.println("The sum is " + (firstNumber + secondNumber));
+        System.out.println("The sum is " + (sum));
 
-        System.out.println("5 * 4 =" + (firstNumber * secondNumber));
+        int product = firstNumber * secondNumber;
+//        System.out.println("The product is " + (firstNumber * secondNumber));
+        System.out.println("The product is " + (product));
+
+
+
+
 
     }
 }
